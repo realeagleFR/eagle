5776aac555c2882f67378fb8abeb2eb4b1fab693
realeagleFR <168231586+realeagleFR@users.noreply.github.com>
2024-04-26 21:38:34 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,13 @@
-
+@echo off
+:1
+timeout 3 >nul
+echo.
+echo =========================
+echo =         EAGLE         =
+echo =========================
+echo = 1. Minecraft          =
+echo = 2. Roblox             =
+echo = 3. Website            =
+echo = 4. Github             =
+echo =========================
+pause
